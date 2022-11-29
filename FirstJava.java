d25b7b51d1583c67219b458f15fda4de35eacf98
zjawwaad <zjawwaad@yahoo.com>
2022-11-29 14:47:22 -0500

    First Java Commit

---
+++ FirstJava.java
@@ -0,0 +1,9 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Zaynah");
+        System.out.println("I am 30 years old.");
+        System.out.println("My hometown is Edison, NJ");
+    }
+}
+
+
